5f903dc1a5f8dc98a11db3d390ef75892d87dc8a
William Rozier <williamrozier@icloud.com>
2023-07-18 08:44:37 -0400

    angfire import not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment.prod'; // Check the relative path here
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCLeahkdMZXIph1e2gaj0VpabcZkUJafuU",
+        authDomain: "ang-blog-76f3f.firebaseapp.com",
+        projectId: "ang-blog-76f3f",
+        storageBucket: "ang-blog-76f3f.appspot.com",
+        messagingSenderId: "764794259669",
+        appId: "1:764794259669:web:d3b1a095007fe13c87fa5c"
+      }
+    
+};
+
