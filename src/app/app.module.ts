5d14260e41f0fc30597bd0b1fd22d0ccf213318b
William Rozier <williamrozier@icloud.com>
2023-07-18 22:09:33 -0400

    AngularFirestoreModule import throwing error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.prod'; // Check the relative path here
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
